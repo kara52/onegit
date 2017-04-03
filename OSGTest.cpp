87ab304c2030d9b4d78d5226deb85dc4ada318c1
WeiPing <weipinglb@163.com>
2017-04-03 18:08:20 +0800

    hehe

---
+++ OSGTest.cpp
@@ -0,0 +1,19 @@
+// OSGTest.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+//int _tmain(int argc, _TCHAR* argv[])
+//{
+//	return 0;
+//}
+
+#include<osgViewer/Viewer> 
+#include<osgDB/ReadFile> 
+int main(int argc, char **argv)
+{
+	osgViewer::Viewer viewer;
+	viewer.setSceneData(osgDB::readNodeFile("cow.osg"));
+	return viewer.run();
+}
